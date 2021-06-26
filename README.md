5ed79fad4038ab58380fbdf32b8102cf442ab211
ChristianSSMartins <contato.christian.martins@gmail.com>
2021-06-26 05:14:01 -0300

    Final commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pokedex-App
+Aplicação Android que consome a API https://pokeapi.co/ para criação de uma Pokedex
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,56 @@
+import { PokeServiceService } from './../services/poke-service.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  
+  offset = 0;
+  pokemon = [];
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
+
+  constructor(private pokeService: PokeServiceService) {}
+
+  ngOnInit() {
+    this.loadPokemon();
+  }
+
+  loadPokemon(loadMore = false, event?) {
+    if(loadMore) {
+      this.offset += 25;
+    }
+    this.pokeService.getPokemon(this.offset).subscribe(res => {
+      console.log('result: ', res);
+      this.pokemon = [...this.pokemon, ...res];
+
+      if(event) {
+        event.target.complete();
+      }
+
+      if(this.offset == 25) {
+        this.infinite.disabled = true;
+      }
+    })
+  }
+
+  onSearchChange(e) {
+    let value = e.detail.value;
+
+    if(value == '') {
+      this.offset = 0;
+      this.loadPokemon();
+      return;
+    }
+
+    this.pokeService.findPokemon(value).subscribe(res => {
+      this.pokemon = [res];
+    }, err => {
+      this.pokemon = [];
+    });
+  }
+
+}
